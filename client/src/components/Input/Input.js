e29be485a4f13c339701317779cab0105ecc4a40
Will Morgan <morgan88will@gmail.com>
2022-04-17 17:53:43 -0400

    test

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
 
-import './Input.css';
+import "./Input.css";
 
 const Input = ({ setMessage, sendMessage, message }) => (
   <form className="form">
     <input
       className="input"
       type="text"
-      placeholder="Type a message..."
+      placeholder="Enter a message..."
       value={message}
       onChange={({ target: { value } }) => setMessage(value)}
-      onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+      onKeyPress={(event) =>
+        event.key === "Enter" ? sendMessage(event) : null
+      }
     />
-    <button className="sendButton" onClick={e => sendMessage(e)}>Send</button>
+    <Button
+      className="sendButton"
+      disabled={!message}
+      onClick={(e) => sendMessage(e)}
+    >
+      Send
+    </Button>
   </form>
-)
+);
 
 export default Input;
